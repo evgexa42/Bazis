4b391af7387f2b55f65e1e109555fed7e3e34f37
evgexa42 <69402443+evgexa42@users.noreply.github.com>
2025-11-06 21:50:22 +0200

    Delete Импорт фрагмента.js

--- Импорт фрагмента.js
+++
@@ -1,8 +0,0 @@
-let file = 'C:\\Users\\жщшо\\Documents\\Bazis\\#_Фрагменты\\Фрагменты элементов мебели\\Fasad_R.fr3d';
-let frag = OpenFurniture(file);
-if (frag) {
-    let obj = frag.Make();
-    obj.Owner = Model.Temp;
-    obj.Build();
-    obj.ElasticResize({x: 200, y: 600, z: 0});
-}
